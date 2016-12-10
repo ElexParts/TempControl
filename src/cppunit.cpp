eda3ad3247c3f06b62751719e1851c0541ce0bd1
Romualdo Dasig <das@trueapex.com>
2016-12-10 18:25:39 +0800

    Move libraries to new directory.

--- src/cppunit.cpp
+++ src/cppunit.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-#include "SevenSegmentDisplay.h"
+//#include "SevenSegmentDisplay.h"
 
 using namespace std;
 
-int main()
-{
-  int ledPins;
-  
-  SevenSegmentDisplay sevenSegmentDisplay(5, 3);
-  
-  ledPins = sevenSegmentDisplay.getLedPins();
-  
-  cout << ledPins << endl;
+int main() {
+//  int ledPins;
+//
+//  SevenSegmentDisplay sevenSegmentDisplay(5, 3);
+//
+//  ledPins = sevenSegmentDisplay.getLedPins();
+//
+//  cout << ledPins << endl;
+
+	cout << "Hello World!" << endl;
+	return 0;
 }
